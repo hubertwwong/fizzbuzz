13e6156a2bc16907cf6a9bcd9403c72617876989
Hubert Wong <hubertwwong@gmail.com>
2011-03-30 15:23:24 -0700

    rails. create new project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fizzbuzz::Application.config.secret_token = 'a6c6aa26360909b826027c537da0dd071de53bb5ad4c6f72f6dbcfb7d6b2129090b0cfb4b13f82a8cdd7dc68384cd357c337e9143669244cb4bd34b289b3fed1'
